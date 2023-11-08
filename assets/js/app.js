c809e81ff110e4fc9dfc704175d495f1988fad25
prince-372 <pmor18314@gmail.com>
2023-11-08 15:53:53 +0530

    add hover

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,7 +18,7 @@ let cross1 = document.querySelector(".cross1");
 let cross2 = document.querySelector(".cross2");
 let cross3 = document.querySelector(".cross3");
 // navbar
-function navbar() {
+function nav() {
     view.classList.toggle("end-0");
     cross1.classList.toggle("crossLine1");
     cross2.classList.toggle("crossLine2");
@@ -26,7 +26,7 @@ function navbar() {
     document.querySelector("body").classList.toggle("overflow-hidden");
 }
 
-    function navbar2() {
+    function nav2() {
         view.classList.toggle("end-0");
         cross1.classList.toggle("crossLine1");
         cross2.classList.toggle("crossLine2");
